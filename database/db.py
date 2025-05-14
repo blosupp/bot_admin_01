e8619836dac11ab96e9329bd58ce601ad1a45c51
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-15 01:39:24 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-# SQLite (в будущем можно заменить на PostgreSQL)
-DATABASE_URL = "sqlite+aiosqlite:///db.sqlite3"
+# ⬇ Переход в корень проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE_PATH = os.path.join(BASE_DIR, "db.sqlite3")
+DATABASE_URL = f"sqlite+aiosqlite:///{DATABASE_PATH}"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
