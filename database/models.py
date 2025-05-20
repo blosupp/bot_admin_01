fe247cd86758ef366bde5f273767721522220cea
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-20 23:25:34 +0200

    Update models.py

--- database/models.py
+++ database/models.py
@@ -4,12 +4,14 @@ from database.db import Base
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}  # 👈 обязательно для перезапуска
 
     id = Column(BigInteger, primary_key=True)
-    username = Column(String)
-    role = Column(String, default="user")
+    username = Column(String, nullable=True)
+    role = Column(String, default="client")
     balance = Column(Integer, default=0)
     post_count = Column(Integer, default=0)
+    use_memory = Column(Boolean, default=True)  # ✅ добавили
 
     channels = relationship("Channel", back_populates="owner")
 
@@ -40,3 +42,12 @@ class Message(Base):
     user_id = Column(BigInteger, index=True)
     role = Column(String)  # 'user' или 'assistant'
     content = Column(String)
+
+class TempPost(Base):
+    __tablename__ = "temp_posts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(BigInteger, index=True)
+    file_id = Column(String)
+    caption = Column(String)  # итоговый текст
+    original = Column(String)  # исходная подпись от пользователя
