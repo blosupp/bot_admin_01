3a69bbb74a8bd1194b975a3664fb8b95a771d64e
Khongor Indzheev <indjeev.kh@gmail.com>
2025-06-08 19:30:28 +0200

    1

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ from bot.config import BOT_TOKEN
 from bot.handlers import user  # импортируем user router
 from bot.handlers import user, prompt, generate  # <— добавили prompt
 from aiogram.fsm.storage.memory import MemoryStorage
-from bot.handlers import user, prompt, generate, channels, post
+from bot.handlers import user, prompt, channels
 from database.db import engine, Base
 from bot.handlers import prompts
 from scheduler.post_scheduler import scheduler, check_scheduled_posts
@@ -16,16 +16,14 @@ from bot.handlers.generate.generate_text import router as generate_text_router
 from bot.handlers.generate.generate_video import router as generate_video_router
 from bot.handlers.generate.generate_photo import router as generate_photo_router
 
-
-
-from bot.handlers.photo_generate import router as photo_router
-from bot.handlers.post_video import router as video_router
-
+from bot.handlers.post.post_photo import router as post_photo_router
+from bot.handlers.post.post_video import router as post_video_router
+from bot.handlers.post.post_text import router as post_text_router
+from bot.handlers.post.scheduler_control import router as scheduler_control_router
 
 
 
 from bot.handlers.help import router as help_router
-from bot.handlers.post_scheduler_control import router as scheduler_control_router
 from bot.handlers import schedule
 from bot.handlers import superadmin
 from aiogram.types import CallbackQuery
@@ -47,10 +45,11 @@ dp.include_router(scheduler_control_router)
 dp.include_router(help_router)
 dp.include_router(generate_video_router)
 dp.include_router(generate_photo_router)
-dp.include_router(video_router)
 dp.include_router(generate_text_router)
-dp.include_router(photo_router)
-dp.include_router(post.router)
+dp.include_router(post_video_router)
+dp.include_router(post_photo_router)
+dp.include_router(post_text_router)
+dp.include_router(scheduler_control_router)
 dp.include_router(chat.router)
 dp.include_router(user.router)
 dp.include_router(prompt.router)
